d496be2fb933b0f0a940eaa4f0fa9398afa4fda5
Niko Berdzenishvili <nikolozisia@gmail.com>
2023-03-22 16:20:03 +0400

    Initial Commit

---
+++ SplitWise/SplitWiseApp.swift
@@ -0,0 +1,17 @@
+//
+//  SplitWiseApp.swift
+//  SplitWise
+//
+//  Created by Niko on 22.03.23.
+//
+
+import SwiftUI
+
+@main
+struct SplitWiseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
